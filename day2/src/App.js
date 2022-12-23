d9530d18658e0647577be57ee807e6acb51be7af
Joel Chris <joel.b.chris@gmail.com>
2022-12-23 12:57:46 +0530

    test

---
+++ day2/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Sharedcomponents/Header/Header';
+import Footer from './Sharedcomponents/Footer/Footer';
+
+function App() {
+  return (<>
+    <Header/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
